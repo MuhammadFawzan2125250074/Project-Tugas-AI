d13e2763936e89c98ee3c7f49a4576658d58c863
MuhammadFawzan2125250074 <fawzan_mhd@mhs.mdp.ac.id>
2023-05-15 09:34:56 +0700

    update
    Co-Authored-By: Azzar Rizky <90712252+zarazzar@users.noreply.github.com>

--- DiagnosaTHT_K6/src/diagnosatht_k6/GejalaDialami.java
+++ DiagnosaTHT_K6/src/diagnosatht_k6/GejalaDialami.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package diagnosatht_k6;
 
-/**
- *
- * @author user
- */
-public class GejalaDialami {
+public class GejalaDialami extends NamaPenyakit {
     
 }
